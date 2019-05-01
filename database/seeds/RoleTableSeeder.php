d352c814b6f7ca5a068e788ac007a5bfa2ad8579
Enes Okutan <enes.okutan@hotmail.com>
2019-05-01 02:56:43 +0000

    created RoleTableSeeder to seed

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+     	$role_working = new Role();
+        $role_working->name = 'Worker';
+        $role_working->description('A worker from employees');
+        $role_working->save();
+
+        $role_working = new Role();
+        $role_working->name = 'Department chef';
+        $role_working->description('Chief of workers');
+        $role_working->save();
+
+        $role_working = new Role();
+        $role_working->name = 'Admin';
+        $role_working->description('Senior management');
+        $role_working->save();   
+    }
+}
