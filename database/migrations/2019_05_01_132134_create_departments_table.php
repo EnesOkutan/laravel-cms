44854d215c4cc48dedb1660ec96a79eb96222474
Enes Okutan <enes.okutan@hotmail.com>
2019-05-01 15:11:55 +0000

    Added timestamps column to roles and departments table

--- database/migrations/2019_05_01_132134_create_departments_table.php
+++ database/migrations/2019_05_01_132134_create_departments_table.php
@@ -19,7 +19,7 @@ class CreateDepartmentsTable extends Migration
             $table->string('name');
             $table->string('description');
             $table->timestamps();
-        });
+        });s
     }
 
     /**
