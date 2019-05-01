708617f07f23494ac3cf5a3a403e930a8d43dd2f
Enes Okutan <enes.okutan@hotmail.com>
2019-05-01 13:21:19 +0000

    Added department_id column to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('username')->unique();
             $table->string('password');
+            $table->integer('department_id');
             $table->rememberToken();
             $table->timestamps();
 
