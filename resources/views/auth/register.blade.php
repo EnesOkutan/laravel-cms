1eaa134168fa380bf32b408b77527c255e77797f
Enes Okutan <enes.okutan@hotmail.com>
2019-05-01 20:15:42 +0000

    added validate to surname

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -31,7 +31,7 @@
                             <div class="col-md-6">
                                 <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus>
 
-                                @error('name')
+                                @error('surname')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
