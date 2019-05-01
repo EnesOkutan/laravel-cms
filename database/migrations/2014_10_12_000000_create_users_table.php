4933132e1ee14e39fe3fb70f67a910a6ec4e1814
Enes Okutan <enes.okutan@hotmail.com>
2019-05-01 18:32:14 +0000

    added email column instead of username column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,8 @@ class CreateUsersTable extends Migration
             $table->unsignedInteger('role_id');
             $table->string('name');
             $table->string('surname');
-            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->unsignedInteger('department_id');
             $table->rememberToken();
