047b02015edfd13c98d477de1f29b173722faac1
Enes Okutan <enes.okutan@hotmail.com>
2019-05-02 10:44:03 +0000

    added checkrole middleware to check role

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if($request->user()->hasRole('Worker')){
+            return redirect('/');
+        }
+
+        if($request->user()->hasRole('Department chef') || $request->user()->hasRole('Admin'))
+        {
+            return redirect('/admin/dashboard');
+        }
+
+        return $next($request);
+    }
+}
