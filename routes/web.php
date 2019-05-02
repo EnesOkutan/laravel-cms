206960cc198dc75f6e194113103c47a25d0ec049
Enes Okutan <enes.okutan@hotmail.com>
2019-05-02 11:36:01 +0000

    fixed middleware problems and added to route

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,10 @@
 |
 */
 
-Route::get('/', 'HomeController@index');
+Route::get('/', function(){
+	return view('home');
+});
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('check.role');
