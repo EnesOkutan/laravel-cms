9a4c39ad6d97819fbef3841386110de34b9dc15f
Enes Okutan <enes.okutan@hotmail.com>
2019-05-03 01:27:28 +0000

    data from database and added to posts.blade.php page

--- resources/views/admin/posts.blade.php
+++ resources/views/admin/posts.blade.php
@@ -1,1 +1,28 @@
 @extends('layouts.admin')
+@section('content')
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Worker</th>
+      <th scope="col">Departman</th>
+      <th scope="col">Title</th>
+      <th scope="col">Düzenle / Sil</th>
+    </tr>
+  </thead>
+  <tbody>
+  	@foreach($posts as $post)
+    <tr>
+      <th scope="row">{{ $post->id }}</th>
+      <td>{{ $post->user()->first()->name . " " . $post->user()->first()->surname }}</td>
+      <td>{{ $post->department()->first()->name }}</td>
+      <td>{{ $post->title }}</td>
+      <td colspan="2">
+      	<button type="button" class="btn btn-success mr-2">Düzenle</button>
+      	<button type="button" class="btn btn-danger">Sil</button>
+      </td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+@endsection
