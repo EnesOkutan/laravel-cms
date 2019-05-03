cbd28a267ec83d3acd5cd220c52a548ff2a925f3
Enes Okutan <enes.okutan@hotmail.com>
2019-05-03 01:24:52 +0000

    data from database and added to departments.blade.php page

--- resources/views/admin/departments.blade.php
+++ resources/views/admin/departments.blade.php
@@ -1,1 +1,23 @@
 @extends('layouts.admin')
+@section('content')
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Düzenle / Sil</th>
+    </tr>
+  </thead>
+  <tbody>
+  	@foreach($departments as $department)
+    <tr>
+      <th scope="row">{{ $department->id }}</th>
+      <td>{{ $department->name }}</td>
+      <td>{{ $department->description }}</td>
+      <td colspan="2"><button type="button" class="btn btn-success mr-2">Düzenle</button><button type="button" class="btn btn-danger">Sil</button></td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+@endsection
