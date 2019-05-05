2b68f1a55320f35e4c8904ea6c22a9f55c78323d
Enes Okutan <enes.okutan@hotmail.com>
2019-05-05 23:21:30 +0000

    added Request parameter to functions

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,35 +12,35 @@ use App\User;
 
 class AdminController extends Controller
 {
-    public function allPosts()
+    public function allPosts(Request $request)
     {
     	return view('admin.posts',[
     		'posts' => Post::orderBy('id','desc')->paginate(10)
     	]);
     }
 
-    public function allDepartments()
+    public function allDepartments(Request $request)
     {
     	return view('admin.departments',[
     		'departments' => Department::orderBy('id')->paginate(10)
     	]);
     }
 
-    public function allManagers()
+    public function allManagers(Request $request)
     {
     	return view('admin.managers',[
     		'managers' => Manager::orderBy('id')->paginate(10)
     	]);
     }
 
-    public function allRoles()
+    public function allRoles(Request $request)
     {
     	return view('admin.roles',[
     		'roles' => Role::orderBy('id')->paginate(10)
     	]);
     }
 
-    public function allWorkers()
+    public function allWorkers(Request $request)
     {
         return view('admin.workers',[
             'workers' => User::orderBy('id')->paginate(10)
