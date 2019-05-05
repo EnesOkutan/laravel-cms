40146624a5e64d2e9d4d66a7b13e009469ea8ac7
Enes Okutan <enes.okutan@hotmail.com>
2019-05-05 23:28:16 +0000

    Edited checkmanager middleware

---
+++ app/Http/Middleware/CheckManager.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class CheckManager
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        if($request->user()->hasRole('Worker'))
+        {
+            Auth::logout();
+            return redirect('login');
+        }
+
+        return $next($request);
+    }
+}
