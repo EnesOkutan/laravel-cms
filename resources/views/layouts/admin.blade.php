e971ed5cae70efd9d6246bd19c3e50e1fff76e38
Enes Okutan <enes.okutan@hotmail.com>
2019-05-07 15:44:09 +0000

    Added links, Edited title and Added 'welcome user'

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,22 +12,22 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <title>Hello, world!</title>
+    <title>Dashboard</title>
   </head>
   <body>
 <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Admin Panel</a>
+      <a class="navbar-brand" href="#">Hoşgeldiniz {{ Auth::user()->name . " " . Auth::user()->surname }}</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="{{ url('posts') }}">Home
+            <a class="nav-link" href="{{ url('/') }}">Home
                   <span class="sr-only">(current)</span>
-                </a>
+            </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="{{ url('admin/posts') }}">Posts</a>
