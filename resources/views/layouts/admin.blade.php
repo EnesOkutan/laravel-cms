b3151032e44eb4f08de6a59501bf29605f53451c
Enes Okutan <enes.okutan@hotmail.com>
2019-05-07 18:05:43 +0000

    Added error message

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -51,6 +51,15 @@
     </div>
   </nav>
   <div class="container mt-5">
+    @if($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        @foreach($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+    @endif
     @yield('content')
   </div>
 
