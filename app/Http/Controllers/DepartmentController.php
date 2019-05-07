fa0cfe7b1f06dc25002ddeccfa84b4843c0cb73b
Enes Okutan <enes.okutan@hotmail.com>
2019-05-07 17:52:43 +0000

    Added store function and validation to department

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class DepartmentController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+    	$request->validate([
+    		'name' => 'required|unique:departments',
+    		'description' => 'required|max:255',
+    	]);
+    }
 }
