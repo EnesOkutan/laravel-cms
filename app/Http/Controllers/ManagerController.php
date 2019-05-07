873efd5ee2bc601e937e1c44562fd21e6a77fa1c
Enes Okutan <enes.okutan@hotmail.com>
2019-05-07 17:53:08 +0000

    Added store function and validation to Manager

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class ManagerController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+    	$request->validate([
+    		'user' => 'require',
+    		'department' => 'require',
+    	]);
+    }
 }
