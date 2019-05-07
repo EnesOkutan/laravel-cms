51721d3d789b3e309427f1d10ac7ad2b63bf9e7f
Enes Okutan <enes.okutan@hotmail.com>
2019-05-07 17:15:16 +0000

    Added request validation to form

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Role;
 
 class RoleController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+    	$request->validate([
+    		'name' => 'required|unique:roles',
+    		'description' => 'required|max:255',
+    	]);
+    }
 }
