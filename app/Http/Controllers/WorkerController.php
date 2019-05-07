604768da0f3451b4d5b1967c669e37cd7f3540d2
Enes Okutan <enes.okutan@hotmail.com>
2019-05-07 17:54:35 +0000

    Added store function and validation to Worker

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class WorkerController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+    	$request->validate([
+    		'role' => 'require',
+    		'department' => 'require',
+    		'name' => 'require',
+    		'surname' => 'require',
+    		'email' => 'require|email',
+    		'password' => 'require|min:8',
+    		'completePassword' => 'require',
+    	]);
+    }
 }
