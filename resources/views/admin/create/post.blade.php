ca86d35927a7cd4485dc14803b996030b04fd42e
Enes Okutan <enes.okutan@hotmail.com>
2019-05-07 18:05:19 +0000

    Fixed problem and Edited error message

--- resources/views/admin/create/post.blade.php
+++ resources/views/admin/create/post.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 @section('content')
-<form class="py-5" action="{{ url('admin/create/post') }}" method="POST">
+<form class="py-5" method="POST" action="{{ url('admin/store/post') }}">
   @csrf
   <div class="form-group row">
     <label for="departments" class="col-sm-2 col-form-label">Department:</label>
